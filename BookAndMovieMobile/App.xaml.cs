32ba7c5f963b97d53af9f665c74fbb6adf0ee845
HALİL İBRAHİM KÜÇÜKDOĞRU <halilibrahimkucukdogru@posta.mu.edu.tr>
2021-10-14 21:47:58 +0300

    Add TabbedPage

--- BookAndMovieMobile/App.xaml.cs
+++ BookAndMovieMobile/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookAndMovieMobile.View.TabMenu;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace BookAndMovieMobile
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new TabControl();
         }
 
         protected override void OnStart()
